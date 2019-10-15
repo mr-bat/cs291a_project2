8af5429c879d59adc100609cdcb46f2578ae489c
Abtin Bateni <abtinbateni@gmail.com>
2019-10-15 00:52:16 -0700

    add GET / and GET /files/

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
 gem 'google-cloud-storage'
+gem 'json'
 gem 'puma'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
-require 'sinatra'
+# frozen_string_literal: true
 
-get '/' do
-  "Hello World\n"
+require 'sinatra'
+require 'digest'
+require 'json'
+require 'google/cloud/storage'
+storage = Google::Cloud::Storage.new(project_id: 'cs291-f19')
+bucket = storage.bucket 'cs291_project2', skip_lookup: true
+
+def validate_sha256(string)
+  !string.match(/\A[a-zA-Z0-9]{2}\/[a-zA-Z0-9]{2}\/[a-zA-Z0-9]{60}\z/).nil?
 end
 
-post '/' do
-  require 'pp'
-  PP.pp request
-  "POST\n"
+get '/' do
+  redirect to('/files/')
+end
+
+get '/files/' do
+  valid_hashes = []
+  files = bucket.files
+  files.all do |file|
+    valid_hashes << file.name if validate_sha256 file.name
+  end
+  print valid_hashes
+  content_type :json
+  valid_hashes.to_json
 end
