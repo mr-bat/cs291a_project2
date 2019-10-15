fd3dc52940cf60ede7f8a8ca0ccbca7bb0ec441f
Abtin Bateni <abtinbateni@gmail.com>
2019-10-15 00:59:15 -0700

    fix GET /files/

--- app.rb
+++ app.rb
@@ -19,9 +19,30 @@ get '/files/' do
   valid_hashes = []
   files = bucket.files
   files.all do |file|
-    valid_hashes << file.name if validate_sha256 file.name
+    valid_hashes << file.name.gsub('/','') if validate_sha256 file.name
   end
   print valid_hashes
   content_type :json
   valid_hashes.to_json
+end
+
+post '/upload' do
+  unless params[:file] &&
+         (tmpfile = params[:file][:tempfile]) &&
+         (name = params[:file][:filename])
+    @error = 'No file selected'
+    return haml(:upload)
+  end
+  warn "Uploading file, original name #{name.inspect}"
+  while blk = tmpfile.read(65_536)
+    # here you would write it to its final location
+    warn blk.inspect
+  end
+  'Upload complete'
+end
+
+post '/' do
+  require 'pp'
+  PP.pp request
+  "POST\n"
 end
